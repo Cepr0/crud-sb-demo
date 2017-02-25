53bed0b3c846f521eb2b5f4475a5e9ee8a6d5de1
Cepr0 <cepr0@ukr.net>
2017-02-25 15:32:25 +0200

    add / delete / create user implemented

--- src/main/java/io/github/cepr0/RootController.java
+++ src/main/java/io/github/cepr0/RootController.java
@@ -3,9 +3,7 @@ package io.github.cepr0;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Cepro, 2017-02-22
@@ -37,5 +35,18 @@ public class RootController {
     public String save(User user) {
         repo.save(user);
         return "redirect:/";
+    }
+    
+    @GetMapping("/user/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        repo.delete(id);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/user")
+    public String create(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "edit";
     }
 }
