e39dde20904c2ff2957bbb9d743f70260ea1fa21
Cepr0 <cepr0@ukr.net>
2017-02-26 00:34:48 +0200

    filter user list

--- src/main/java/io/github/cepr0/RootController.java
+++ src/main/java/io/github/cepr0/RootController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 /**
  * @author Cepro, 2017-02-22
  */
@@ -22,6 +25,7 @@ public class RootController {
     public String users(Model model) {
         Iterable<User> users = repo.findAll();
         model.addAttribute("users", users);
+        model.addAttribute("search", "");
         return "users";
     }
 
@@ -48,5 +52,14 @@ public class RootController {
         User user = new User();
         model.addAttribute("user", user);
         return "edit";
+    }
+    
+    @PostMapping("/filter")
+    public String search(HttpServletRequest req, Model model) {
+        String search = req.getParameter("search");
+        List<User> users = repo.findByNameAndEmail(search);
+        model.addAttribute("users", users);
+        model.addAttribute("search", search);
+        return "users";
     }
 }
--- src/main/java/io/github/cepr0/UserRepo.java
+++ src/main/java/io/github/cepr0/UserRepo.java
@@ -1,9 +1,17 @@
 package io.github.cepr0;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @author Cepro, 2017-02-22
  */
 public interface UserRepo extends CrudRepository<User, Integer> {
+    List<User> findByNameContainingIgnoreCase(String name);
+    
+    @Query("select u from User u where lower(concat(u.name, u.email)) like concat('%', lower(?1), '%')")
+    List<User> findByNameAndEmail(String search);
 }
